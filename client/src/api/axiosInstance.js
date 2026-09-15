57d5cd00b27938dd0e3b92faa26d95f5bc8fbb68
vishalkhade25 <blessedvishu25@gmail.com>
2026-09-15 13:55:24 +0530

    Fixed Error

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL : import.meta.env.VITE_API_BASE_URL
-    baseURL : "http://localhost:5000/api"
+    baseURL : import.meta.env.VITE_API_BASE_URL
+    // baseURL : "http://localhost:5000/api"
 });
 
 axiosInstance.interceptors.request.use((config)=>{
